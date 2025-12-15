62d96971eb68b386f4afec9806d8d69f1dd496c1
CyberSphinxx <johnlemargonzales@gmail.com>
2025-12-15 23:17:34 +0800

    config(css): add blob animation keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,30 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Inter', 'sans-serif'],
-                serif: ['Merriweather', 'serif'],
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        animation: {
+            blob: "blob 7s infinite",
+        },
+        keyframes: {
+            blob: {
+                "0%": {
+                    transform: "translate(0px, 0px) scale(1)",
+                },
+                "33%": {
+                    transform: "translate(30px, -50px) scale(1.1)",
+                },
+                "66%": {
+                    transform: "translate(-20px, 20px) scale(0.9)",
+                },
+                "100%": {
+                    transform: "translate(0px, 0px) scale(1)",
+                },
             },
         },
     },
-    plugins: [],
+},
+plugins: [],
 }
